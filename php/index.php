bd8c3962f93263a0ec55f3693b4181f5117eb090
isum450 <132076877+isum450@users.noreply.github.com>
2023-05-25 00:17:43 +0900

    Add files via upload

---
+++ php/index.php
@@ -0,0 +1,49 @@
+<?php
+/*echo "Hello world<h1>php 하기 시렁</h1>";
+echo 20020509;
+
+print(12345);
+print("Hello World<h1>php 하고 시퍼</h1>");
+
+$A = "이수민";
+echo $A;
+
+define("Fuck", 184829);
+const FFuck = "19283";
+echo Fuck;
+echo FFuck;
+
+$Num1 = 1523;
+$Num2 = 27;
+$Sum = $Num1+ $Num2;
+echo $Sum;
+
+$Num1 = "1523";
+$Num2 = "27";
+$Sum = $Num1 + $Num2;
+
+echo $Sum;
+
+$a = "abc";
+$b = "def";
+echo $a."Fuck".$b;
+
+define("CHAR", "대한민국");
+echo CHAR."은 자유민주국가";
+
+const CHAR = "미국";
+
+*/
+$a = "남자"
+
+if($a == "남자"){
+    echo "남자입니다";
+}else if($a == "여자"){
+    echo "여자입니다";
+}
+else {
+    echo "중성입니다";
+}
+
+
+?>
